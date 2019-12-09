7fe0e85eee8c3a1910fcf2bd69c6bc27fb0d9766
nikovornehm@hotmail.de <56032588+acse-ncv19@users.noreply.github.com>
2019-12-09 16:24:11 +0000

    merged with Niko branch

---
+++ armageddon/no_matter.py
@@ -0,0 +1,23 @@
+from solver import Planet
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+earth = Planet(atmos_func='exponential')
+df = earth.solve_atmospheric_entry(10, 20e3, 3000, 1e5, 45)
+print(df)
+fig = plt.figure(figsize=(5,5))
+plt.plot(df['velocity'], df['altitude'])
+plt.xlabel('velocity')
+plt.ylabel('altitude')
+plt.show()
+
+
+'''
+0: velocity
+1: mass
+2: angle
+3: altitude
+4: distance
+5: radius
+'''
