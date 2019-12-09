6c30bb606b9041aac5baed6659a518d0870dd1c4
Helen Situ <hvs119@imperial.ac.uk>
2019-12-09 14:50:14 +0000

    implemented probability distributions

--- armageddon/ensemble.py
+++ armageddon/ensemble.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
+from scipy.special import erf
 
+#from .solver import *
 
 def solve_ensemble(
         planet,
@@ -42,19 +44,35 @@ def solve_ensemble(
         DataFrame with columns of any parameters that are varied and the
         airburst altitude
     """
+    
+    result = planet.solve_atmospheric_entry(radius=fiducial_impact['radius'],
+                                            angle=fiducial_impact['angle'],
+                                            strength=fiducial_impact['strength'],
+                                            velocity=fiducial_impact['velocity'],
+                                            density=fiducial_impact['density'])
 
     for var in variables:
-        # Remove these as you implement each distribution
         if var == 'radius':
-            raise NotImplementedError
+            radius_series = result['radius']
+            radius_dist = (radius_series - rmin) / (rmax - rmin)
         if var == 'angle':
-            raise NotImplementedError
+            angle = result['angle']
+            angle_dist = np.cos(angle)**2
         if var == 'strength':
-            raise NotImplementedError
+            str = result['strength']
+            smin = 1e3
+            smax = 1e7
+            s_dist = np.log(str/smin) / np.log(smax/smin)
         if var == 'velocity':
-            raise NotImplementedError
+            v = result['velocity']
+            a = 1.1e4
+            # at infinity:
+            v_dist = erf(v/(a*np.sqrt(2))) \
+                     - (v/a)*np.exp(-(v**2)/(2*a**2))*np.sqrt(2/np.pi)
+            # I don't know what the bit about impact velocity means
         if var == 'density':
-            raise NotImplementedError
+            rho = result['density']
+            rho_dist = 0.5*(1+erf((rho-3e3)/(1e3*np.sqrt(2))))
 
     # Implement your ensemble function here
 
