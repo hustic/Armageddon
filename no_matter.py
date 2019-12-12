8932ca429bd157ada358ba5c3c2bc80d34d4a19b
nikovornehm@hotmail.de <56032588+acse-ncv19@users.noreply.github.com>
2019-12-12 21:35:25 +0000

    yo

--- no_matter.py
+++ no_matter.py
@@ -1,99 +1,46 @@
+import numpy as np
 from solver import Planet
-'''import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 from analytical import anal_sol
-'''
 import time
+import scipy_test
 
-<<<<<<< HEAD
-earth = Planet(atmos_func='exponential')
-df, dic = earth.impact(10, 20e3, 3000, 1e5, 45)
-#print(df)
-#print(dic)
-#anal_df = anal_sol()
-print(anal_df)
-fig = plt.figure(figsize=(7,7))
-plt.plot(df['dedz'], df['altitude'])
-plt.xlabel('altitude')
-plt.ylabel('time')
-#plt.ylim(0,20000)
-plt.grid()
-plt.show()
-=======
-def costly_func():
-    earth = Planet(atmos_func='exponential', atmos_filename='../data/AltitudeDensityTable.csv')
-    df, out = earth.impact(10, 20e3, 3000, 10e5, 45, num_scheme='RK', fragmentation=True, dt=0.05, init_altitude=80000)
-    return df, out
-start_time = time.time()
-df, out = costly_func()
-end_time = time.time()
-print(end_time - start_time)
-#H_plot = np.array(df.altitude)
-#anal_df = anal_sol(H_plot, 10, 20e3, 3000, 3000, 45, 100e3, False)
-#print(out)
-#data = pd.read_csv('../data/AltitudeDensityTable.csv', header=None, delim_whitespace=True, skiprows=6)
-#print(data)
-'''schemes = [
-            'EE',
-            'IE',
-            'MIE',
-            'RK'
-            ]
-'''
-'''dts = [0.1, 0.05, 0.01, 0.005, 0.001]
-dt_rms = []
-for dt in dts:
-    RMS_velocity = []
-    RMS_dedz = []
-    for scheme in schemes:
-        df, dic = earth.impact(5, 10e3, 1200, 1e5, 30, 100e3, dt, False, False, scheme)
-        H_plot = np.array(df.altitude)
-        anal_df = anal_sol(H_plot, 5, 10e3, 1200, 1e5, 30, 100e3, False)
-        RMS_velocity.append(np.sqrt(1/len(df) * sum((anal_df.velocity - df.velocity)**2)))
-    dt_rms.append(RMS_velocity)
->>>>>>> master
+radius = [5, 10]
+velocity = [10e3, 20e3]
+strength = [3000, 10e5]
+angle = [30, 45]
+density = [1200, 3000]
 
-dt_rms = np.array(dt_rms).T
-'''
-'''fig = plt.figure(figsize=(8, 8))
+earth = Planet(g=0, Cl=0, Ch=0)
+#df_vec, out_vec = earth.impact(radius, velocity, density, strength, angle, num_scheme='RK', fragmentation=True)
+
+
+
+sci_res = scipy_test.sci_sol(5, 10e3, 1200, 10e5, 45, num_scheme='RK45', fragmentation=False, g=0, C_L=0, C_H=0, dt=0.01)
+
+df, out = earth.impact(5, 10e3, 1200, 10e5, 45, num_scheme='RK', fragmentation=False, dt=0.01, ensemble=False)
+anal_res = anal_sol(df.altitude, 5, 10e3, 1200, 10e5, 45)
+
+fig = plt.figure(figsize=(8, 8))
 ax1 = plt.subplot(211)
 ax2 = plt.subplot(212)
-#ax3 = plt.subplot(213)
-'''
-#äax1.scatter(df.altitude, df.velocity, marker='.', color='r', label='num')
-#ax1.plot(H_plot, anal_df.velocity, label='anal')
-'''ax1.plot(dts, dt_rms[0], label='EE')
-ax1.plot(dts, dt_rms[1], label='IE')
-ax1.plot(dts, dt_rms[2], label='MIE')
-ax1.plot(dts, dt_rms[3], label='RK')
-ax1.set_xlabel('altitude', fontsize=14)
-ax1.set_ylabel('v', fontsize=14)
-#ax1.set_title("RMSs")
-#ax1.set_ylim(0, 40e3)
+
+ax1.scatter(df.velocity, df.altitude, label='numeric', marker='.', color='r')
+ax1.plot(sci_res.velocity, sci_res.altitude, label='scipy', color='b')
+ax1.plot(anal_res.velocity, anal_res.altitude, label='anal', color='g')
+ax1.set_ylabel('velocity')
+ax1.set_xlabel('altitude')
+ax1.set_ylim(0,1e5)
 ax1.grid()
-ax1.legend()'''
-'''
-ax2.scatter(df.altitude, df.dedz, marker='.', color='r', label='num')
-#ax2.plot(H_plot, anal_df.dedz, label='anal')
-ax2.set_xlabel('altitude', fontsize=14)
-ax2.set_ylabel('dedz', fontsize=14)
-#ax2.set_title("numerical")
+ax1.legend()
+
+ax2.scatter(df.dedz, df.altitude, label='numeric', marker='.', color='r')
+ax2.plot(sci_res.dedz, sci_res.altitude, label='scipy', color='b')
+ax2.plot(anal_res.dedz, anal_res.altitude, label='anal', color='g')
+ax2.set_ylabel('dedz')
+ax2.set_xlabel('altitude')
+ax2.set_ylim(0,1e5)
 ax2.grid()
 ax2.legend()
-#ax2.set_xlim((0, 40e3))'''
-'''
-ax3.plot(data.iloc[0], data.iloc[1], label='tabular density')
-ax3.grid()
-ax3.legend()
-'''
-#plt.show()
 
-'''
-0: velocity
-1: mass
-2: angle
-3: altitude
-4: distance
-5: radius
-'''
+plt.show()
